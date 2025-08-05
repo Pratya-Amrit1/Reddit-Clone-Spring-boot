3397452396ac7d0c19990f9b1c3e87ea4b009da7
Pratya <pratya192@gmail.com>
2025-08-05 10:10:20 +0530

    Add controllers and DTOs for authentication, comments, posts, subreddits, and votes in the Reddit Clone API

---
+++ src/main/java/com/programming/pratya/springredditclone/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.programming.pratya.springredditclone.dto;
+
+import com.programming.pratya.springredditclone.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
